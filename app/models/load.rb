9bac05ac7bf94ca3887d0abf89c0098249b8447c
treverandersen <trever.r.andersen@gmail.com>
2019-03-28 14:01:26 -0500

    setup rspec tests and added validations to the load model

--- app/models/load.rb
+++ app/models/load.rb
@@ -1,2 +1,4 @@
 class Load < ApplicationRecord
+	validates :load_in, presence: true
+	validates :load_out, presence: true
 end
--- spec/models/load_spec.rb
+++ spec/models/load_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Load, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do 
+    it "has a valid load factory" do 
+      expect(build(:load)).to be_valid
+    end
+
+    it "is invalid without a load_in" do 
+      expect(build(:load, load_in: nil)).not_to be_valid
+    end
+
+    it "is invalid without a load_out" do 
+      expect(build(:load, load_out: nil)).not_to be_valid 
+    end
+  end
+
+  describe "relationships" do 
+    it "belongs to an account" do
+    end
+
+    it "belongs to a driver" do
+    end
+
+    it "belongs to a bin" do
+    end
+
+    it "belongs to a field" do 
+    end
+  end
+
+  describe "instance methods" do 
+    it "has a test weight selected" do
+    end
+  end
+
+  describe "class methods" do 
+    it "has calculated bushels" do
+    end
+  end
 end
